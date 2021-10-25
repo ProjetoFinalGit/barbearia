41ade77d16b74dd908fb849eeb6eb56dc83d5f6f
vgvitor@hotmail.com <84208595+vitorgaspar72@users.noreply.github.com>
2021-10-25 18:24:25 -0300

    Vitor
    Atualização do banco de dados

--- projetoetb.sql
+++ projetoetb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 20-Out-2021 às 12:36
+-- Tempo de geração: 25-Out-2021 às 21:20
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.3.21
 
@@ -38,25 +38,7 @@ CREATE TABLE IF NOT EXISTS `agendamento` (
   `idCliente` int(11) NOT NULL,
   `idUsuario` int(11) NOT NULL,
   PRIMARY KEY (`idAgendamento`,`idCliente`,`idUsuario`),
-  KEY `fk_Agendamento_Cliente1_idx` (`idCliente`),
   KEY `fk_Agendamento_usuario1_idx` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cliente`
---
-
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `cpf` varchar(45) NOT NULL,
-  `endereco` varchar(45) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `dataNascimento` date NOT NULL,
-  PRIMARY KEY (`idCliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -132,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `perfil` (
 
 INSERT INTO `perfil` (`idPerfil`, `nome`, `dataCadastro`) VALUES
 (1, 'Administrador', '2021-10-13'),
-(2, 'Diretor', '2021-10-12'),
+(2, 'Gerente', '2021-10-12'),
 (3, 'Funcionário', '2021-10-11'),
 (4, 'Cliente', '2021-10-10');
 
@@ -178,26 +160,32 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(64) NOT NULL,
-  `login` varchar(24) NOT NULL,
+  `login` varchar(45) NOT NULL,
   `senha` varchar(124) NOT NULL,
   `status` int(11) NOT NULL,
   `idPerfil` int(11) NOT NULL,
+  `telefone` varchar(45) NOT NULL,
+  `dataNascimento` date DEFAULT NULL,
+  `endereco` varchar(45) NOT NULL,
+  `cpf` varchar(45) NOT NULL,
   PRIMARY KEY (`idUsuario`,`idPerfil`),
   KEY `fk_usuario_Perfil_idx` (`idPerfil`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`idUsuario`, `nome`, `login`, `senha`, `status`, `idPerfil`) VALUES
-(9, 'Vitor Gabriel', 'vgvitor@hotmail.com', '123321', 1, 1),
-(10, 'Miguelzin', 'vitorphilos1@hotmail.com', '050220', 2, 6),
-(11, 'Orso', 'teste@etb.com.br', '123321123', 1, 7),
-(12, 'Sophia', 'vgvitor@hotmail.com', '123321', 1, 6),
-(13, 'Diego', 'seculurum@hotmail.com', '050220', 1, 2),
-(14, 'Miguel', 'miguel@hotmail.com', '123', 1, 3),
-(15, 'Vitor', 'teste@etb.com.br', '9873465251987', 1, 2);
+INSERT INTO `usuario` (`idUsuario`, `nome`, `login`, `senha`, `status`, `idPerfil`, `telefone`, `dataNascimento`, `endereco`, `cpf`) VALUES
+(9, 'Vitor Gabriel', 'vgvitor@hotmail.com', '123321', 1, 1, '', NULL, '', '060.828.811-09'),
+(10, 'Miguelzin', 'vitorphilos1@hotmail.com', '050220', 2, 6, '', NULL, '', ''),
+(11, 'Orso', 'teste@etb.com.br', '123321123', 1, 7, '', NULL, '', ''),
+(13, 'Diego', 'seculurum@hotmail.com', '050220', 1, 2, '', NULL, '', ''),
+(14, 'Miguel', 'miguel@hotmail.com', '123', 1, 3, '', NULL, '', ''),
+(15, 'Vitor', 'teste@etb.com.br', '9873465251987', 1, 2, '', NULL, '', ''),
+(16, 'Orso Miguel', 'miguelito@hotmail.com', '91851540', 1, 4, '', NULL, '', ''),
+(18, 'Duduzin Henrique', 'duduzin@gmail.com', '91851540', 1, 4, '', NULL, '', ''),
+(22, 'Novo Teste Implementação', 'testedeverificacao@teste.com.br', '984198749864', 1, 4, '(61) 99628-0717', '1996-12-31', 'Qs 05, Rua 310, Costa Vitória', '564.984.191-84');
 
 --
 -- Restrições para despejos de tabelas
@@ -207,7 +195,6 @@ INSERT INTO `usuario` (`idUsuario`, `nome`, `login`, `senha`, `status`, `idPerfi
 -- Limitadores para a tabela `agendamento`
 --
 ALTER TABLE `agendamento`
-  ADD CONSTRAINT `fk_Agendamento_Cliente1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Agendamento_usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
